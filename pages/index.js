737c9733f2e1ebe397a3b8e41778b71351a6e67a
George Treviranus <geowtrev@gmail.com>
2022-06-18 22:28:12 -0500

    chore: adds configs and netlify cms

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,10 @@ export default class Home extends Component {
     return (
       <>
         <Head>
-          <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+          <script
+            defer
+            src="https://identity.netlify.com/v1/netlify-identity-widget.js"
+          ></script>
         </Head>
         <article>
           <h1>{title}</h1>
