12264e0446ec096b88edd7e8f5938d7306dfb01e
George Treviranus <geowtrev@gmail.com>
2022-06-19 22:03:45 -0500

    fix: fetches main article content from page api endpoint

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 module.exports = {
   webpack: (cfg) => {
-    cfg.module.rules.push({
-      test: /\.md$/,
-      loader: "frontmatter-markdown-loader",
-      options: { mode: ["react-component"] },
-    })
-    cfg.experiments = { topLevelAwait: true }
+    cfg.experiments.topLevelAwait = true
     return cfg
   },
 }
--- pages/essays/index.js
+++ pages/essays/index.js
@@ -1,8 +1,5 @@
-// get static list of slugs from public/essays/*.md
-// list them in increments of 10, use "load more" button
-// import PropTypes from "prop-types"
 import Link from "next/link"
-import { getEssayData } from "../../lib/get-essays"
+import { getEssayData } from "../../lib/get-essay-metadata"
 
 export default function Essays({ essayData }) {
   const { entries } = essayData
@@ -11,14 +8,19 @@ export default function Essays({ essayData }) {
       <h1>Essays</h1>
       <ul>
         {entries.map((entry) => {
-          const { path, date, slug } = entry
-          const { title } = entry.article.metadata
+          const {
+            path,
+            date,
+            slug,
+            metadata: { title, description },
+          } = entry
 
           return (
             <li key={slug}>
               <h3>
                 <Link href={path}>{title}</Link>
               </h3>
+              <p>{description}</p>
               <time dateTime={date}>{date}</time>
             </li>
           )
