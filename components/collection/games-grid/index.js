4a3a66e17082881f2646fb1732b71759f0ff58a9
George Treviranus <geowtrev@gmail.com>
2022-09-05 01:17:07 -0500

    fix: pluralize search results and hide count when no results to show

--- components/collection/games-grid/index.js
+++ components/collection/games-grid/index.js
@@ -2,13 +2,18 @@ import { GameCell } from "../game-cell"
 import styles from "./styles.module.scss"
 
 export function GamesGrid({ games, label, id }) {
+  const length = games.length
   return (
     <>
       <h2 id={id}>{label}</h2>
       <div className={styles.listInfo}>
-        <p>{games.length} games shown</p>
+        {games.length > 0 && (
+          <p>
+            {length} {length === 1 ? "game" : "games"} shown
+          </p>
+        )}
       </div>
-      {games.length ? (
+      {length > 0 ? (
         <ul aria-labelledby={id} className={styles.gameGrid} aria-label={label}>
           {games.map((data) => (
             <GameCell key={`${data.name}-${data.grade}`} data={data} />
