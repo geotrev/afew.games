183b3b38bc48851e1f41f38826fdba3743aedb3a
George Treviranus <geowtrev@gmail.com>
2022-09-17 13:50:05 -0500

    refactor: move gtag script to dSIHTML

--- pages/_app.js
+++ pages/_app.js
@@ -57,15 +57,18 @@ export default function App({ Component, pageProps }) {
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
         strategy="afterInteractive"
       />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}');
-        `}
-      </Script>
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}');
+          `,
+        }}
+      ></Script>
       {renderMeta()}
       <SiteHeader />
       <Component {...pageProps} />
