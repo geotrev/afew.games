56c9f9d00df9057ff162e97b5481d1b1933ea74d
George Treviranus <3946669+geotrev@users.noreply.github.com>
2023-04-02 12:06:13 -0500

    feat: add copyright (#182)
    * feat: add copyright
    
    * fix: spacing/style
    
    * fix: notice color

--- app/components/site-footer/index.tsx
+++ app/components/site-footer/index.tsx
@@ -4,23 +4,34 @@ import styled from "styled-components"
 import { SubscribeForm } from "../subscribe-form"
 
 const StyledFooter = styled.footer`
-  border: 2px solid ${(p) => p.theme.colors.tertiary2};
   margin: 1rem 0 2rem 0;
+`
+
+const StyledSubscribe = styled.div`
+  border: 2px solid ${(p) => p.theme.colors.tertiary2};
   padding: 1rem;
+  margin-block-end: 1rem;
 `
 
 const StyledHeader = styled.h2`
   margin-block-end: 0.5rem;
 `
 
+const StyledCopyrightNotice = styled.p`
+  color: rgba(${(p) => p.theme.colors.triplets.text}, 0.7);
+`
+
 export function SiteFooter() {
   return (
     <StyledFooter>
-      <StyledHeader>Subscribe to A Few Games!</StyledHeader>
-      <p>
-        Get occasional emails about game collecting. Unsubscribe at any time.
-      </p>
-      <SubscribeForm />
+      <StyledSubscribe>
+        <StyledHeader>Subscribe to A Few Games!</StyledHeader>
+        <p>
+          Get occasional emails about game collecting. Unsubscribe at any time.
+        </p>
+        <SubscribeForm />
+      </StyledSubscribe>
+      <StyledCopyrightNotice className="text-xs">Copyright &copy; A Few Games</StyledCopyrightNotice>
     </StyledFooter>
   )
 }
