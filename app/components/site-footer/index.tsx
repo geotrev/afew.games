c10edfc7ac76de6c19d3d8bd49f6ca024fac0d2f
george treviranus <geowtrev@gmail.com>
2023-12-31 12:54:01 -0600

    chore: moves social links to components

--- app/components/site-footer/index.tsx
+++ app/components/site-footer/index.tsx
@@ -1,3 +1,4 @@
+import { SocialLinks } from "../social-links"
 import { SubscribeForm } from "../subscribe-form"
 
 export function SiteFooter() {
@@ -19,47 +20,7 @@ export function SiteFooter() {
         <p className="text-xs">
           <span>&copy; 2022-{new Date().getFullYear()} A Few Games</span>
         </p>
-        <span className="flex gap-2 pe-2">
-          <a
-            className="btn-accent btn-sm btn block py-0.5"
-            href="https://buymeacoffee.com/afew.games"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="Buy a coffee for George, the maintainer of A Few Games"
-          >
-            <picture>
-              <source
-                type="image/svg+xml"
-                media="(max-width: 600px)"
-                width="18px"
-                srcSet="/bmc-logo-small.svg 18w"
-              />
-              <source type="image/svg+xml" srcSet="/bmc-full-logo.svg" />
-              <img
-                src="/bmc-full-logo.svg"
-                alt=""
-                role="presentation"
-                height="18px"
-                width="110px"
-              />
-            </picture>
-          </a>
-          <a
-            className="btn-ghost btn-sm btn flex items-center py-0.5"
-            href="https://github.com/geotrev/afew.games"
-            target="_blank"
-            rel="noopener noreferrer"
-            aria-label="View the code repository of A Few Games"
-          >
-            <img
-              src="/github-mark-white.svg"
-              alt=""
-              role="presentation"
-              height="22px"
-              width="22px"
-            />
-          </a>
-        </span>
+        <SocialLinks />
       </div>
     </footer>
   )
