8515585dcf3fbb66b303e69f16703166b3c99b7e
George Treviranus <geowtrev@gmail.com>
2024-04-20 14:06:17 -0500

    chore: add suspense boundary

--- app/app.tsx
+++ app/app.tsx
@@ -1,13 +1,15 @@
-import { PropsWithChildren } from "react"
+import { PropsWithChildren, Suspense } from "react"
 import { SiteHeader } from "./components/site-header"
 import { SiteFooter } from "./components/site-footer"
 
 export function App({ children }: PropsWithChildren) {
   return (
-    <div className="mx-auto mb-0 mt-4 max-w-screen-md p-4">
-      <SiteHeader />
-      <main>{children}</main>
-      <SiteFooter />
-    </div>
+    <Suspense>
+      <div className="mx-auto mb-0 mt-4 max-w-screen-md p-4">
+        <SiteHeader />
+        <main>{children}</main>
+        <SiteFooter />
+      </div>
+    </Suspense>
   )
 }
