37fd5fa2125c20b87f48c9df8a353a4e3a8c8766
George Treviranus <geowtrev@gmail.com>
2024-08-23 10:42:40 -0500

    fix: move client about page content to separate component

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,6 @@
-import Link from "next/link"
 import { PageHeading } from "../components/page-heading"
 import { BASE_TITLE } from "utils/constants"
-import { Paragraph } from "@zendeskgarden/react-typography"
+import { Content } from "./components/content"
 
 export const metadata = {
   alternates: {
@@ -13,55 +12,9 @@ export const metadata = {
 
 export default function Page() {
   return (
-    <div className="prose max-w-full">
+    <>
       <PageHeading>About</PageHeading>
-      <Paragraph>
-        <em>A Few Games</em> started as a simple blog & collection website. I
-        enjoyed the idea of sharing my game collection, but quickly after
-        building the initial version, I wanted to share my (at the time) private
-        database of video game print data, not just the specific games I own.
-      </Paragraph>
-      <Paragraph>
-        Fast forward to today, the site has switched to a public & open source
-        database, but with a secondary blog component. My passion is still to
-        share the interesting & enjoyable history of physical video games.
-      </Paragraph>
-      <Paragraph>
-        In other words, I want to answer questions like: What is the distinct
-        chronology of a physical game&apos;s printing? What signifiers do
-        publishers exercise when revising games? How scarce are these different
-        print variants?
-      </Paragraph>
-      <Paragraph>
-        Finally, where do you, the reader/contributor, fit into this? As
-        mentioned above, the database is fully open source. You can view both
-        the raw data and code at the{" "}
-        <Link
-          className="link"
-          href="https://github.com/geotrev/afew.games"
-          rel="noreferrer noopener"
-          target="_blank"
-        >
-          GitHub repository
-        </Link>
-        . I aim to facilitate an environment where folks can help build a free,
-        accessible, and transparent resource, together.
-      </Paragraph>
-      <Paragraph>Thanks for reading and I hope to see you around!</Paragraph>
-      <Paragraph className="italic">
-        PS: Do you want to see the games I own? I have{" "}
-        <Link
-          className="link"
-          href="https://www.instagram.com/a.few.games/"
-          rel="noopener noreferrer"
-        >
-          an Instagram
-        </Link>{" "}
-        for that! I&apos;d love to see your games too. Thanks if you decide to
-        give a follow. On a related note, sign up for occasional emails with
-        updates on new blog posts and database additions. I promise not to spam
-        you. ;)
-      </Paragraph>
-    </div>
+      <Content />
+    </>
   )
 }
--- app/components/site-header/index.tsx
+++ app/components/site-header/index.tsx
@@ -1,7 +1,6 @@
 "use client"
 
 import { usePathname } from "next/navigation"
-import { DEFAULT_THEME, ThemeProvider } from "@zendeskgarden/react-theming"
 import Logo from "./afewgames.svg"
 import { SocialLinks } from "../social-links"
 import Link from "next/link"
@@ -34,43 +33,36 @@ export function SiteHeader() {
   const pathname = usePathname()
 
   return (
-    <ThemeProvider
-      theme={{
-        ...DEFAULT_THEME,
-        colors: { ...DEFAULT_THEME.colors, base: "dark" },
-      }}
-    >
-      <header className="flex items-start bg-transparent px-0 pb-8 pt-0">
-        <div className="flex-1" itemScope itemType="https://schema.org/Blog">
-          <Link
-            href={Routes.HOME_PATH}
-            className="h-auto w-16 p-0 hover:bg-transparent md:w-24"
-          >
-            <Logo />
-            <span className="sr-only">Go to home page</span>
-          </Link>
-        </div>
-        <nav className="flex flex-col gap-6 md:flex-row">
-          <div className="flex items-center gap-6">
-            {NavigationItems.map(({ label, isActive, route }) => {
-              const active = isActive(pathname)
+    <header className="flex items-start bg-transparent px-0 pb-8 pt-0">
+      <div className="flex-1" itemScope itemType="https://schema.org/Blog">
+        <Link
+          href={Routes.HOME_PATH}
+          className="h-auto w-16 p-0 hover:bg-transparent md:w-24"
+        >
+          <Logo />
+          <span className="sr-only">Go to home page</span>
+        </Link>
+      </div>
+      <nav className="flex flex-col gap-6 md:flex-row">
+        <div className="flex items-center gap-6">
+          {NavigationItems.map(({ label, isActive, route }) => {
+            const active = isActive(pathname)
 
-              return (
-                <Link
-                  key={label}
-                  href={route}
-                  aria-current={active ? "true" : undefined}
-                >
-                  {label}
-                </Link>
-              )
-            })}
-          </div>
-          <div className="flex items-center justify-end gap-6">
-            <SocialLinks />
-          </div>
-        </nav>
-      </header>
-    </ThemeProvider>
+            return (
+              <Link
+                key={label}
+                href={route}
+                aria-current={active ? "true" : undefined}
+              >
+                {label}
+              </Link>
+            )
+          })}
+        </div>
+        <div className="flex items-center justify-end gap-6">
+          <SocialLinks />
+        </div>
+      </nav>
+    </header>
   )
 }
