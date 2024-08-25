61ea8efbc6f3fe891c322b4020cd3d0815ef3b5b
George Treviranus <geowtrev@gmail.com>
2024-08-25 14:58:01 -0500

    feat: sticky header

--- app/components/site-header/index.tsx
+++ app/components/site-header/index.tsx
@@ -35,37 +35,41 @@ export function SiteHeader() {
   const pathname = usePathname()
 
   return (
-    <header className="navbar items-start bg-transparent px-0 pb-8 pt-0">
-      <div className="flex-1" itemScope itemType="https://schema.org/Blog">
-        <a
-          href={Routes.HOME_PATH}
-          className="btn btn-ghost h-auto w-16 p-0 hover:bg-transparent md:w-24"
-        >
-          <Image alt="A Few Games" src={Logo} width={94} height={76} />
-        </a>
-      </div>
-      <nav className="flex flex-col items-end gap-4">
-        <ul className="menu menu-horizontal menu-sm gap-2 rounded-box bg-base-200 sm:menu-md">
-          {NavigationItems.map(({ label, isActive, route }) => {
-            const active = isActive(pathname)
+    <>
+      <header className="navbar sticky top-0 z-50 items-center bg-base-100 px-0 pb-2 pt-4">
+        <div className="flex-1" itemScope itemType="https://schema.org/Blog">
+          <a
+            href={Routes.HOME_PATH}
+            className="btn btn-ghost h-auto w-16 p-0 hover:bg-transparent md:w-24"
+          >
+            <Image alt="A Few Games" src={Logo} width={94} height={76} />
+          </a>
+        </div>
+        <nav className="flex flex-col items-end gap-4">
+          <ul className="menu menu-horizontal menu-sm gap-2 rounded-box bg-base-200 sm:menu-md">
+            {NavigationItems.map(({ label, isActive, route }) => {
+              const active = isActive(pathname)
 
-            return (
-              <li key={label}>
-                <Link
-                  href={route}
-                  aria-current={active ? "true" : undefined}
-                  className={cn("font-bold", {
-                    "!bg-blue-500 !text-black": active,
-                  })}
-                >
-                  {label}
-                </Link>
-              </li>
-            )
-          })}
-        </ul>
+              return (
+                <li key={label}>
+                  <Link
+                    href={route}
+                    aria-current={active ? "true" : undefined}
+                    className={cn("font-bold", {
+                      "!bg-blue-500 !text-black": active,
+                    })}
+                  >
+                    {label}
+                  </Link>
+                </li>
+              )
+            })}
+          </ul>
+        </nav>
+      </header>
+      <div className="flex justify-end py-2">
         <SocialLinks />
-      </nav>
-    </header>
+      </div>
+    </>
   )
 }
