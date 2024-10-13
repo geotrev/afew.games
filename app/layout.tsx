5b01fa11e1bf74d9a6b862ed864c5cc5b9b507c8
George Treviranus <geowtrev@gmail.com>
2024-10-13 10:48:24 -0500

    feat: updates favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,21 @@ export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
       <head>
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-48x48.png"
+          sizes="48x48"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <meta name="apple-mobile-web-app-title" content="MyWebSite" />
+        <link rel="manifest" href="/site.webmanifest" />
         <meta
           name="keywords"
           content="Video Games, Games, Collectibles, Graded Games, Sealed Video Games"
