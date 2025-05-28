68c26d9362f9f11f9cf86dd45b39907f8d3ab9f1
George Treviranus <3946669+geotrev@users.noreply.github.com>
2025-05-28 14:55:39 -0500

    chore: use escapable character

--- src/app/_components/site-footer/index.tsx
+++ src/app/_components/site-footer/index.tsx
@@ -18,7 +18,7 @@ export function SiteFooter() {
         <p className="text-xs">
           <span>&copy; 2022-{new Date().getFullYear()} A Few Games</span>
           &nbsp;&bull;&nbsp;
-          <span>Designed & developed by <a href="https://rightwarp.com" className="link">Right Warp, LLC</a></span>
+          <span>Designed &amp; developed by <a href="https://rightwarp.com" className="link">Right Warp, LLC</a></span>
         </p>
         <SocialLinks />
       </div>
